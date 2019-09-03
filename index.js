2fecc35b678fc506d93f8a945491abe4c79f21ef
ayunas <ayunas@gmail.com>
2019-09-03 15:05:08 -0500

    finisehd endpoint and basic message handling logic for all endpoints

--- index.js
+++ index.js
@@ -44,8 +44,33 @@ server.delete("/api/users/:id", (req, res) => {
   dbHelper
     .remove(req.params.id)
     .then(deleted => {
-      console.log(deleted);
-      res.status(204).send(deleted);
+      if (deleted) {
+        console.log(deleted);
+        res.status(204).json(deleted);
+      } else {
+        res.status(404).json({
+          message:
+            "that user with id " +
+            req.params.id +
+            " doesn't exist in the database"
+        });
+      }
+    })
+    .catch(err => res.status(500).json(err.message));
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const user = req.body;
+  console.log(user);
+  dbHelper
+    .update(id, user)
+    .then(() => {
+      dbHelper
+        .findById(id)
+        .then(user => res.status(200).json(user))
+        .catch(err => res.status(404).json(err.message));
     })
     .catch(err => res.status(500).json(err.message));
 });
